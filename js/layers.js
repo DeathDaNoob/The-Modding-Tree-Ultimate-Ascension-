0d98afe8d1ed1c837826743826da2b48c8bc8185
Axolotf <deathdanoob@gmail.com>
2026-06-03 22:05:17 +0100

    Added Content

--- js/layers.js
+++ js/layers.js
@@ -1,28 +1,59 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("cultivation", {
+    name: "Cultivation", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#0bc4e1",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "cultivation level", // Name of prestige currency
+    baseResource: "qi", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+        if (hasUpgrade('cultivation',13)) mult = mult.times(upgradeEffect(this.layer,13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        return new Decimal(1.2)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "c", description: "C: Reset for cultivation level", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades : {
+        11:{
+            title: "Foundation Building",
+            description: "Doubles Your Qi Gain",
+            cost: new Decimal(2),
+        },
+        12:{
+            title: "Train In Peace",
+            description: "Multiplies Qi Gain By 2*Cultivation Level",
+            cost: new Decimal(10),
+            effect() {
+                let mult = player[this.layer].points.times(2)
+                if (mult.gte(1)) return mult
+                else return new Decimal(1)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+        13:{
+            title: "MORE QI MORE TRAIN",
+            description: "Multiplies Cultivation By Log(Qi)",
+            cost: new Decimal(250),
+            effect() { 
+                return player.points.add(1).log(10);
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+    },
+    
+
+
     layerShown(){return true}
 })
--- js/mod.js
+++ js/mod.js
@@ -40,7 +40,10 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
+	
 	let gain = new Decimal(1)
+	if (hasUpgrade('cultivation', 11)) gain = gain.times(2)
+	if (hasUpgrade('cultivation', 12)) gain = gain.times(upgradeEffect('cultivation',12))
 	return gain
 }
 
